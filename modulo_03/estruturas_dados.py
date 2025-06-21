e99d61aaf73546d554df18559f20c30e340b6cbe
kauani em vocacao <kauanilopess70@gmail.com>
2025-06-21 18:09:51 -0300

    Create estruturas_dados.py

---
+++ modulo_03/estruturas_dados.py
@@ -0,0 +1,7 @@
+frutas = ["maçã", "banana", "laranja"]
+for fruta in frutas:
+    print("Fruta:", fruta)
+
+pessoa = {"nome": "Ana", "idade": 17}
+print("Nome:", pessoa["nome"])
+print("Idade:", pessoa["idade"])
