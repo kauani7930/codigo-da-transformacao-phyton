5c99798fd6269c659977549932bf59df8981f13f
kauani em vocacao <kauanilopess70@gmail.com>
2025-08-10 19:04:40 -0300

    Update expressoes-matematica.py

--- modulo_02/expressoes-matematica.py
+++ modulo_02/expressoes-matematica.py
@@ -6,3 +6,28 @@ print("Subtração:", num1 - num2)
 print("Multiplicação:", num1 * num2)
 print("Divisão:", num1 / num2)
 print("Resto da divisão:", num1 % num2)
+
+
+##comparacao
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+if n1 > n2:
+    print("O maior número é:", n1)
+elif n2 > n1:
+    print("O maior número é:", n2)
+else:
+    print("Os dois números são iguais.")
+
+##classificar idad
+idade = int(input("Digite sua idade: "))
+
+if idade <= 12:
+    print("Criança")
+elif idade <= 17:
+    print("Adolescente")
+elif idade <= 59:
+    print("Adulto")
+else:
+    print("Idoso")
+
